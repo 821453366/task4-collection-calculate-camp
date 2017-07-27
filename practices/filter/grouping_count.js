a65a38736a5cc1b72317f0d4bf7c63d6a73c67cf
821453366 <821453366@qq.com>
2017-07-27 14:32:47 +0800

    filter第四道题

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function grouping_count(collection) {
+  let result_Array = {};
 
-  //在这里写入代码
+  let result = Array.from(new Set(collection));
+  result.filter(item => {
+    let num = 0;
+    collection.forEach(data => {
+      if (data == item) {
+        num++;
+      }
+    })
+    result_Array[item] = num;
+  })
+
+  return result_Array;
 }
 
 module.exports = grouping_count;
