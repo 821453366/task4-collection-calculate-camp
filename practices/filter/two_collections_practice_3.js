dfff880e7b6f5de6c78ad487e237c60e2c52dcde
821453366 <821453366@qq.com>
2017-07-27 15:50:20 +0800

    完成filter 第七道题

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
+  let result = [];
 
-  //在这里写入代码
+  collection_a.forEach(item => {
+    collection_b.forEach(data => {
+      if (item % data ===0) {
+        result.push(item);
+      }
+    })
+  })
+
+  return result;
 }
 
 module.exports = choose_divisible_integer;
