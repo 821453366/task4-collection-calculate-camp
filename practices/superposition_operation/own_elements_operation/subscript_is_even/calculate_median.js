3bc637024e48c42c96985651a03c07bb63463894
821453366 <821453366@qq.com>
2017-07-28 17:54:26 +0800

    subscript第二题

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,16 @@
 'use strict';
 var calculate_median = function(collection){
+  var result = [];
+  for (var i in collection) {
+    if (collection[i] % 2 === 0) {
+      result.push(collection[i]);
+    }
+  }
 
+  if (collection.length % 2 !== 0) {
+    return collection[parseInt((collection.length + 1) / 2)] - 1;
+  } else {
+    return collection[parseInt(collection.length / 2)];
+  }
 };
 module.exports = calculate_median;
