5a142a6269f512d631bf5eab19114655be0e25bc
821453366 <821453366@qq.com>
2017-07-28 17:36:41 +0800

    interval最后一道题

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,31 @@
 'use strict';
 
 function median_to_letter(collection) {
+  let sum = 0;
+  let result = "";
+  let word = "";
 
-  //在这里写入代码
+  collection.sort((a, b) => a - b).forEach((item, index) => {
+    if (collection.length % 2 == 0) {
+      if (index + 1 === (collection.length / 2) || index + 1 === (collection.length / 2) + 1)
+        sum += Math.ceil(parseInt(item) / 2);
+    } else {
+      if (index + 1 === Math.ceil(collection.length / 2)) {
+        sum += parseInt(item);
+      }
+    }
+  })
+  let number = sum / 26;
+  let num = sum % 26;
+  for (let i = 1; i < number; i++) {
+    word += String.fromCharCode(97);
+  }
+  if (num !== 0)
+    word += String.fromCharCode(num + 96);
+  result+=word
+
+  return result;
+
 }
 
 module.exports = median_to_letter;
