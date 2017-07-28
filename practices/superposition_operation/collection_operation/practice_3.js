04b14992c61d096c0ff22b29039722056dfc9720
821453366 <821453366@qq.com>
2017-07-28 17:21:05 +0800

    collection最后一道题

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
+  let result = 0;
 
-  //在这里写入代码
+  collection.forEach(item => {
+    if (item % 2 != 0) {
+      result += (item * 3 + 5)
+    }
+  })
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
