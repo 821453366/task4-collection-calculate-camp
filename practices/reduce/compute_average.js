b6e2e43e4fe016c507d9449d2433ab8ba2918ea2
821453366 <821453366@qq.com>
2017-07-28 11:20:13 +0800

    reduce第四道题完成

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+ let sum = collection.reduce((value,item)=> value+item);
+
+ return sum/collection.length;
 }
 
 module.exports = compute_average;
