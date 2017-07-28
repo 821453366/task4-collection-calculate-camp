5a433d969b750f9a91d0a1420cc316b80b32bc9c
821453366 <821453366@qq.com>
2017-07-28 14:29:41 +0800

    reduce第五道题

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+  let sum=0;
+  collection.sort((a, b) => a - b).forEach((item, index)=>{
+    if(collection.length %2 ==0){
+      if (index + 1 === (collection.length / 2) || index + 1 === (collection.length / 2) + 1)
+        sum += parseInt(item)/2;
+    }else{
+      if(index+1 === Math.ceil(collection.length/2)){
+        sum += parseInt(item);
+      }
+    }
+  })
+  return sum
 }
 
 module.exports = compute_median;
