b3f417eece8a05651b705de9da25a7958480cab2
821453366 <821453366@qq.com>
2017-07-28 11:40:49 +0800

    reduce第五道题

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  let sum = 0;
+
+  let result = collection.split("->").sort((a, b) => a - b);
+  result.forEach((item, index) => {
+    if (index + 1 === (result.length / 2) || index + 1 === (result.length / 2) + 1)
+      sum += parseInt(item);
+  })
+
+  return sum / 2;
 }
 
 module.exports = compute_chain_median;
