b88b192b998ea08ce3121850e1e7f945b967f3fa
821453366 <821453366@qq.com>
2017-07-28 17:04:44 +0800

    完成collection_operation 第一道题

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function even_to_letter(collection) {
+  let result = [];
 
-  //在这里写入代码
+  collection.forEach(item => {
+    if (item % 2 == 0) {
+      let word = "";
+      let number = item / 26;
+      let num = item % 26;
+      for (let i = 1; i < number; i++) {
+        word += String.fromCharCode(97);
+      }
+      if (num !== 0)
+        word += String.fromCharCode(num + 96);
+      result.push(word)
+    }
+  })
+
+  return result;
 }
 
 module.exports = even_to_letter;
