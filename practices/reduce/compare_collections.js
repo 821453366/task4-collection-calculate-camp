fa70b33341136a150a1225fba773067717078efa
821453366 <821453366@qq.com>
2017-07-28 11:17:32 +0800

    reduce第三道题

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,7 +1,20 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  let num =0;
+
+  collection_a.forEach((item,index)=> {
+    collection_b.forEach(( item_b, index_b) => {
+      if (index === index_b && item === item_b)
+        num++;
+    })
+  })
+  if(num === collection_b.length){
+
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = compare_collections;
