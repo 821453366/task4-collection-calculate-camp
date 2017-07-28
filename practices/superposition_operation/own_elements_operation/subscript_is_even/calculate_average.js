37acde3fbe05194a374ad8dd91261557e6810366
821453366 <821453366@qq.com>
2017-07-28 17:40:51 +0800

    subscript 第一道题

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,15 @@
 'use strict';
-var calculate_average = function(collection){
+var calculate_average = function (collection) {
+  let result = 0;
+  let num = 0;
 
+  collection.forEach((item, index) => {
+    if ((index + 1) % 2 === 0) {
+      num++;
+      result += item;
+    }
+  })
+
+  return result/num;
 };
 module.exports = calculate_average;
